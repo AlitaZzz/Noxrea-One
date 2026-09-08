12eab63d7ea83122bc7960fdb2df2c3637216209
Noxrea <tianyi.lee.0727@gmail.com>
2026-09-09 03:10:25 +0800

    fix(web): 修正 UploadItem 的导入来源

--- web/src/features/canvas/upload/create-nodes-from-files.ts
+++ web/src/features/canvas/upload/create-nodes-from-files.ts
@@ -18,7 +18,8 @@ import {
 import { computeNodeSize, loadMediaDimensions } from "@/lib/utils/image-utils";
 import { isOffline } from "@/lib/utils/upload";
 
-import { detectMediaKind, runMediaUpload, type UploadItem } from "./upload-pipeline";
+import { detectMediaKind, runMediaUpload } from "./upload-pipeline";
+import type { UploadItem } from "./types";
 
 /** 网格列数：多文件拖入 / 上传时按此列数换行排布 */
 const GRID_COLS = 4;
